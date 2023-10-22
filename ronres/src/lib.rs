8450687f81c6e60c3bc2ef8613b5657177636b3c
Michal Kosinski <kosinski.mich@gmail.com>
2023-10-22 14:28:48 +0200

    Increase macros visibility

--- ronres/src/lib.rs
+++ ronres/src/lib.rs
@@ -1,4 +1,7 @@
 pub use ronres_derive::RonResource;
+pub use ronres_derive::RonLoader;
+pub use ronres_derive::RonSaver;
+pub use ronres_derive::ronres_uid_map;
 pub use ronres_defs::traits::RonResource;
 pub use ronres_defs::types::UidMap;
 pub use ronres_defs::serde_gd;
