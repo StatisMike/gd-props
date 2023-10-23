7a854fda16fd4bd21a1a221f7624742c60890379
Michal Kosinski <kosinski.mich@gmail.com>
2023-10-23 23:14:53 +0200

    Fix GdRonSaver

--- ronres-derive/src/ron_saver.rs
+++ ronres-derive/src/ron_saver.rs
@@ -39,7 +39,7 @@ pub fn derive_ron_saver(decl: Declaration) -> Result<TokenStream, venial::Error>
       }
 
       fn get_recognized_extensions(&self, _resource: godot::obj::Gd<godot::engine::Resource>) -> godot::builtin::PackedStringArray {
-        godot::builtin::PackedStringArray::from(&[godot::builtin::GodotString::from("ron")])
+        godot::builtin::PackedStringArray::from(&[godot::builtin::GodotString::from("gdron")])
       }
 
       fn set_uid(&mut self, path: godot::builtin::GodotString, uid: i64) -> godot::engine::global::Error {
@@ -48,57 +48,8 @@ pub fn derive_ron_saver(decl: Declaration) -> Result<TokenStream, venial::Error>
       }
     }
 
-    impl #struct_ident {
-      /// Name under which the object registers in Godot as a singleton
-      pub const SINGLETON_NAME: &'static str = stringify!(#struct_ident);
-
-      fn create_instance()-> godot::obj::Gd<Self> {
-        godot::obj::Gd::<Self>::new_default().upcast()
-      } 
-
-      /// Associated function to retrieve the pointer to object singleton
-      /// as `Gd<ResourceFormatSaver>`.
-      pub fn saver_singleton() -> godot::obj::Gd<Self> {
-        if godot::engine::Engine::singleton()
-          .has_singleton(Self::SINGLETON_NAME.into()) {
-
-          godot::engine::Engine::singleton()
-          .get_singleton(Self::SINGLETON_NAME.into()).unwrap()
-          .cast::<Self>()
-  
-        } else {
-  
-          let object = Self::create_instance();
-          godot::engine::Engine::singleton()
-          .register_singleton(Self::SINGLETON_NAME.into(),object.clone().upcast());
-          object
-        }
-      }
-
-      /// Associated function to register the created `ResourceFormatSaver`
-      /// in Godot's `ResourceSaver`. To be used in `lib.rs` declaration of
-      /// `ExtensionLibrary` implementation.
-      /// 
-      /// ## Example
-      /// 
-      /// ```no_run
-      /// //lib.rs
-      /// 
-      /// struct MyGdExtension;
-      ///
-      /// unsafe impl ExtensionLibrary for MyGdExtension {
-      ///     fn on_level_init(_level: InitLevel) {
-      ///         if _level = InitLevel::Scene {
-      ///             MyRonSaverStruct::register_saver();
-      /// 
-      ///     }   
-      /// }
-      /// ```
-      pub fn register_saver() {
-        let instance = Self::saver_singleton();
-        let saver = &mut godot::engine::ResourceSaver::singleton();
-        saver.add_resource_format_saver(instance.upcast());
-      }
+    impl ronres::traits::GdRonSaver for #struct_ident {
+      const SINGLETON_NAME: &'static str = stringify!(#struct_ident);
     }
   ))
 }
